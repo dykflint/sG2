f73b3fb85145c0ad391571081f7f62f9f0b3ad90
DykFlint <konstantin.tamoev@gmail.com>
2023-05-23 20:08:25 +0200

    Test recognition.stop

--- tools/preachingtool/mobile_version/dev/base/public/script.js
+++ tools/preachingtool/mobile_version/dev/base/public/script.js
@@ -244,7 +244,6 @@ function finishGame(){
     recognition.lang = 'de-DE';
     recognition.start();
     recognition.onresult = function (event) {
-
       if (switchPlaces % 2 == 0){
         // TOOL SPEAKS FIRST 
       // Reset correct answer div 
@@ -265,7 +264,7 @@ function finishGame(){
       // Here is where stuff is being written 
       if (event.results[0].isFinal) {
         audio_img.src = "https://www.filepicker.io/api/file/VyfbFTekQn6m2LEPlNm5";
-        recognition.stop();
+        alarm(recognition.stop());
         let poopScript = transcript;
         poopScript = transcript.replace(/grossen/gi, 'großen').replace(/gross/gi,'groß').replace(/grosse/gi,'große').replace(/grosses/gi,'großes').replace(/weiss/gi,'weiß').replace(/beisse/gi,'beiße').replace(/beisst/gi,'beißt').replace(/beissen/gi,'beißen').replace(/Fussball/gi,'Fußball');
         p.textContent = poopScript;
